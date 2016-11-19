9557c6c4f7176b4b1625ab4eb4c22cd4ea8d2b91
aravila <anderson.avila@gmail.com>
2016-11-19 17:40:05 -0500

    Fix on srmr extractor

--- extract_srmr.py
+++ extract_srmr.py
@@ -18,35 +18,36 @@ def listfolders(pathfeat, pathfeatstats, pathfeatmovstats, pathfeatcombstats):
 
     path = sys.argv[1:][0]
     mode = sys.argv[2:][0]
-    for dirname in os.listdir(path):
-        feat = ""
-        featstats = ""
-        featmovstats = ""
-        featcombstats = ""
-        if os.path.isdir("%s/%s"%(path, dirname)):
-            if mode == '0':
-                print("clean")
-                feat = "%s/%s" % (pathfeat,"clean/arousal")
-                featstats = "%s/%s" % (pathfeatstats,"clean/arousal")
-                featmovstats = "%s/%s" % (pathfeatmovstats,"clean/arousal")
-                featcombstats = "%s/%s" % (pathfeatcombstats,"clean/arousal")
-            elif mode == '1':
-                print("noise")
-                feat = "%s/%s/%s/%s" % (pathfeat,"noise",dirname,"arousal")
-                featstats = "%s/%s/%s/%s" % (pathfeatstats,"noise",dirname,"arousal")
-                featmovstats = "%s/%s/%s/%s" % (pathfeatmovstats,"noise",dirname,"arousal")
-                featcombstats = "%s/%s/%s/%s" % (pathfeatcombstats,"noise",dirname,"arousal")
-            elif mode == '2':
-                print("enhanced")
-                feat = "%s/%s/%s/%s" % (pathfeat,"noise",dirname,"arousal")
-                featstats = "%s/%s/%s/%s" % (pathfeatstats,"noise",dirname,"arousal")
-                featmovstats = "%s/%s/%s/%s" % (pathfeatmovstats,"noise",dirname,"arousal")
-                featcombstats = "%s/%s/%s/%s" % (pathfeatcombstats,"noise",dirname,"arousal")
-            convert(path, dirname, feat, featstats, featmovstats, featcombstats)
+    dirname = ""
+    if mode == '0':
+        feat = "%s/%s" % (pathfeat, "clean/arousal")
+        featstats = "%s/%s" % (pathfeatstats, "clean/arousal")
+        featmovstats = "%s/%s" % (pathfeatmovstats, "clean/arousal")
+        featcombstats = "%s/%s" % (pathfeatcombstats, "clean/arousal")
+        convert(path, dirname, feat, featstats, featmovstats, featcombstats)
+    else:
+        for dirname in os.listdir(path):
+            feat = ""
+            featstats = ""
+            featmovstats = ""
+            featcombstats = ""
+            if os.path.isdir("%s/%s" % (path, dirname)):
+                if mode == '1':
+                    feat = "%s/%s/%s/%s" % (pathfeat, "noise", dirname, "arousal")
+                    featstats = "%s/%s/%s/%s" % (pathfeatstats, "noise", dirname, "arousal")
+                    featmovstats = "%s/%s/%s/%s" % (pathfeatmovstats, "noise", dirname, "arousal")
+                    featcombstats = "%s/%s/%s/%s" % (pathfeatcombstats, "noise", dirname, "arousal")
+                elif mode == '2':
+                    feat = "%s/%s/%s/%s" % (pathfeat, "noise", dirname, "arousal")
+                    featstats = "%s/%s/%s/%s" % (pathfeatstats, "noise", dirname, "arousal")
+                    featmovstats = "%s/%s/%s/%s" % (pathfeatmovstats, "noise", dirname, "arousal")
+                    featcombstats = "%s/%s/%s/%s" % (pathfeatcombstats, "noise", dirname, "arousal")
+                convert(path, dirname, feat, featstats, featmovstats, featcombstats)
+
 
 def get_no_examples(mf, no_examples = 7501):
     if len(mf) < no_examples:
-        tmp = np.reshape(mf[len(mf) - 1], (1, len(mf[len(mf) - 1])))
+        tmp = np.reshape(mf[len(mf) - 1], (1, mf.shape[1], mf.shape[2]))
         tmp = np.repeat(tmp, no_examples - len(mf), axis=0)
         mf = np.vstack((mf, tmp))
     else:
@@ -54,6 +55,8 @@ def get_no_examples(mf, no_examples = 7501):
     return mf
 
 def write_arff(path, file, features):
+    if not os.path.exists(path):
+        os.makedirs(path)
     f = open("%s/%s"%(path, file), "w")
     f.write("@RELATION %s\n"%(file[:-5]))
     f.write("\n")
@@ -68,8 +71,7 @@ def write_arff(path, file, features):
     for row in range(0, len(features)):
         str = ""
         for col in range(0, len(features[0])):
-            str += "%d, "%(features[row][col])
-        print(str[:-2])
+            str += "%f, "%(features[row][col])
         f.write("%s\n" % str[:-2])
 
     f.close()
@@ -81,7 +83,7 @@ def convert(path, dirname, featpath, featstatspath, featmovstatspath, featcombst
     for file in dirs:
         if file.endswith('.wav'):
             print('%s/%s' % (path, file))
-            mf = audio.srmr_audio('', path, file)
+            mf = audio.srmr_audio(path, file)
             mf = np.einsum('ijk->kij', mf)
             mf = get_no_examples(mf,7501)
             stats = MFStats(mf)
@@ -98,11 +100,17 @@ def convert(path, dirname, featpath, featstatspath, featmovstatspath, featcombst
             mfeatcombstats = stats.moving_stats(mfeatstats, 10)
             write_arff(featcombstatspath, "%s%s"%(file[:-4], ".arff"), mfeatcombstats)
 
-
+# Windows
 pathfeat = 'F:/AVEC/mf_features'
 pathfeatstats = 'F:/AVEC/mf_features_stats'
 pathfeatmovstats = 'F:/AVEC/mf_features_movstats'
 pathfeatcombstats = 'F:/AVEC/mf_features_combstats'
+
+# Mac
+pathfeat = '../../output/mf_features'
+pathfeatstats = '../../output/mf_features_stats'
+pathfeatmovstats = '../../output/mf_features_movstats'
+pathfeatcombstats = '../../output/mf_features_combstats'
 
 listfolders(pathfeat, pathfeatstats, pathfeatmovstats, pathfeatcombstats)
 
